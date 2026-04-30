5acaf1bc4da8e3b2b43e75e3483b59d918d99bcb
AlbertUlysses <albertulysseschavez@gmail.com>
2026-04-29 22:49:32 -0700

    wip

--- src/main.zig
+++ src/main.zig
@@ -5,27 +5,47 @@ const lox_debug = @import("lox_debug.zig");
 const vm = @import("vm.zig");
 const STACK_MAX: u32 = 256;
 
-// pick up here
-fn repl() void {
+fn repl(vmy: vm.VM) void {
     var stdin_buffer: [1024]u8 = undefined; // line
-    var stdout_buffer: [1024]u8 = undefined; // stdout 
+    var stdout_buffer: [1024]u8 = undefined; // stdout
     var stdin_reader = std.fs.File.stdin().writer(&stdin_buffer);
     var stdout_writer = std.fs.File.stdout().reader(&stdout_buffer);
     const stdin = &stdin_reader.interface;
-    const stdout = &stdout_reader.interface;
-    while(true){
+    const stdout = &stdout_writer.interface;
+    while (true) {
         try stdout.print("> ");
         try stdout.flush();
         const line = try stdin.takeDelimiterExclusive('\n');
-        if(std.mem.eql(u8, line, "exit()")){
+        if (std.mem.eql(u8, line, "exit()")) {
             try stdout.writeAll("\n");
             try stdout.flush();
             break;
         }
-        interpret(line);
+        vmy.interpret(line);
     }
 }
-//pick upabove
+
+fn runFile(path: []u8, vmy: vm.VM, ally: std.mem.Allocator) void {
+    const source = readFile(path, ally);
+    const result = vmy.interpret(source);
+    switch (result) {
+        .INTERPRET_COMPILE_ERROR => {
+            std.process.exit(65);
+        },
+        .INTERPRET_RUNTIME_ERROR => {
+            std.process.exit(65);
+        },
+        else => {},
+    }
+}
+
+fn readFile(path: []u8, ally: std.mem.Allocator) void {
+    const cwd = std.fs.cwd();
+    const max_bytes = 16 * 1024 * 1024; // reading in the whole text of the file
+    const text = try cwd.readFileAlloc(ally, path, max_bytes);
+    return text;
+}
+// pick after writing the readFile and now need to check if the text is empty
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}).init;
     defer _ = gpa.deinit();
@@ -37,11 +57,11 @@ pub fn main() !void {
     var vmy = try vm.VM.init(ally, &new_chunk, STACK_MAX);
     _ = vmy.interpret();
 
-    if (std.os.argv.len == 1){
-        repl();
-    }else if (std.os.argv.len == 2){
-        runFile(std.os.argv[1]);
-    } else{
+    if (std.os.argv.len == 1) {
+        repl(vmy);
+    } else if (std.os.argv.len == 2) {
+        runFile(std.os.argv[1], vmy, ally);
+    } else {
         std.debug.print("Usage: clox [path] \n", .{});
         std.process.exit(64);
     }
