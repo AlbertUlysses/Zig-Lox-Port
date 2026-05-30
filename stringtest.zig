e091d320f2af4829a2266c8bc9ef7a2edaba3c75
AlbertUlysses <albertulysseschavez@gmail.com>
2026-05-30 10:48:05 -0700

    clean upfiles

--- stringtest.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() void {
-    var word = "1hello";
-    const word2 = " world\n";
-    word = word[1..];
-    std.debug.print("{s}, {s}", .{ word, word2 });
-}
